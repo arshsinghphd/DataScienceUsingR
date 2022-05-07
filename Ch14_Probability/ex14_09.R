ebc6b26c58e3ae2600826e60a53f520de2931137
arshsinghphd <arshsinghphd@gmail.com>
2022-05-07 09:45:47 -0700

    append ex 14_09

--- Ch14_Probability/ex14_09.R
+++ Ch14_Probability/ex14_09.R
@@ -1,6 +1,26 @@
 library(dplyr)
+library(gtools)
 
-# 10. Two teams, say the Cavs and the Warriors, are playing a seven game 
+
+# Q1 ----------------------------------------------------------------------
+# One ball will be drawn at random from a box containing: 3 cyan balls, 
+# 5 magenta balls, and 7 yellow balls. What is the probability that the ball 
+# will be cyan?
+
+#Calculation
+3/(3+5+7) # =.2
+
+# Monte Carlo Experiment with B of 10^5
+box <- c(rep('c',3),rep('m',5),rep('y',7))
+B <- 10^5
+mean(replicate(B,sample(box,1))=='c') # = ~.20185
+
+
+# Q2 ----------------------------------------------------------------------
+
+
+# Q10 ---------------------------------------------------------------------
+# Two teams, say the Cavs and the Warriors, are playing a seven game 
 # championship series. The first to win four games, therefore, wins the series. 
 # The teams are equally good so they each have a 50-50 chance of winning each 
 # game. If the Cavs lose the first game, what is the probability that they win
@@ -10,12 +30,12 @@ library(dplyr)
 # Cavs win 4 of the next 4 games (4 more, total 5 games)
 # Cavs win 3 of the next 4 games + last game (5 more, total 6 games)
 # Cavs win 3 of the next 5 games + last game (6 more, total 7 games)
-(0.5^4)+(.5^3*.5^1*.5)+(.5^3*.5^2*.5)
+(0.5^4)+(.5^5)+(.5^6)
 #.109375
 
 
 # Q11 ---------------------------------------------------------------------
-#  Confirm the results of the previous question with a Monte Carlo 
+# Confirm the results of the previous question with a Monte Carlo 
 # simulation.
 B <- 10^5
 count=0
